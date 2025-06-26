a0f75c4e59db5dbc9de302198ea6457196e7ec34
ritesh <riteshbhuimbar756@gmail.com>
2025-06-26 15:47:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import ChatbotComponent from "./Components/Chatbot/ChatbotComponents";
+import AboutPage from "./Pages/AboutPage";
+import Contact from "./Pages/Contact";
+import Cources from "./Pages/Cources";
+import HomePage from "./Pages/HomePage";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+
+const App =() =>{
+  return(
+    <div>
+   <Router>
+    <Routes>
+        <Route path="/"element={<HomePage/>}/>
+        <Route path="/about"element={<AboutPage/>}/>
+        <Route path="/contact"element={<Contact/>}/>
+        <Route path="/cources"element={<Cources/>}/>
+      
+    </Routes>
+   </Router>
+   <ChatbotComponent/>
+    </div>
+  )
+}
+export default App;
