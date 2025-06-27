aadde3bed3ce06fe78e214f6265a0f0c50e66da8
ritesh <riteshbhuimbar756@gmail.com>
2025-06-27 15:44:30 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import ChatbotComponent from "./Components/Chatbot/ChatbotComponents";
+import DeveloperInfoPopup from "./Components/DeveloperInfo/DeveloperInfoPopup";
 import AboutPage from "./Pages/AboutPage";
 import Contact from "./Pages/Contact";
 import Cources from "./Pages/Cources";
@@ -6,7 +8,24 @@ import HomePage from "./Pages/HomePage";
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 
 const App =() =>{
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
   return(
+    <>
+    <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Ritesh Bhuimbar"
+          studentPhotoUrl="/images/ritesh.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+
     <div>
    <Router>
     <Routes>
@@ -19,6 +38,7 @@ const App =() =>{
    </Router>
    <ChatbotComponent/>
     </div>
+    </>
   )
 }
 export default App;
